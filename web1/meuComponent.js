0511d7b36ded6079bed1414c7ddcef94c7f6406a
jonhcortez <jonh.ftp@gmail.com>
2018-10-18 19:26:57 -0400

    Create meuComponent.js

---
+++ web1/meuComponent.js
@@ -0,0 +1,9 @@
+var React = require('react'); // importa a lib react
+
+var MeuComponente = React.createClass({
+  render: function(){
+    return (teste);
+  }
+});
+
+module.exports = MeuComponente;
